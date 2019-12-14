d086694f6c20af501239da569bda2a66153661d4
Stefanny <stefanny.lacerda.ismart@gmail.com>
2019-12-14 16:26:51 -0300

    Corrigindo package.jason e index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const router = express.Router();
 
-router.get('/', function (req, res){
+router.get('/', function (req, res) {
     res.status(200).send({
         title: 'Me chama - catálogo de oportunidades',
         version: '1.0.0'
