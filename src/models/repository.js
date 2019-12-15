af82425d4a4fd6b545455a010b68c0ac281951b6
Stefanny <stefanny.lacerda.ismart@gmail.com>
2019-12-15 16:12:33 -0300

    Atualizando configurações do mongoDB

--- src/models/repository.js
+++ src/models/repository.js
@@ -4,11 +4,12 @@ const MONGO_URL = 'mongodb://localhost:27017/OportunidadesApi';
 const connect = () => {
   mongoose.connect(MONGO_URL, { 
     useUnifiedTopology: true,
+    useCreateIndex: true,
     useNewUrlParser: true })
   const connection = mongoose.connection
-  connection.on('error', () => console.error('Erro ao conectar no mongo'))
+  connection.on('error', () => console.error('Erro ao conectar no mongoDB'))
   
-  connection.once('open', () => console.log('Conectamos no mongo!'))
+  connection.once('open', () => console.log('Conectado ao mongoDB'))
 }
 
 module.exports = { connect };
